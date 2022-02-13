3b96dc1817f510f74a666d77f7bb993cdaa093ba
Simeone-Holanda <simeone.10s10@gmail.com>
2022-02-13 07:47:06 -0300

    Versao 1.1 - Retirando valores padroes da lista de tarefas.

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,7 @@ import "./App.css";
 
 const App = () => {
 
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      title: 'Estudar Programação',
-      completed: false,
-    },
-    {
-      id: "2",
-      title: "Ler Livros",
-      completed: true,
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
     const fetchTasks = async () => {
